1b247929f371f536caaf5d50ab1da1e3cbb32174
cacafaca <nemanja.simovic@gmail.com>
2016-04-27 03:41:23 +0200

    Added files via upload

---
+++ Universal Distributed System.mm
@@ -0,0 +1,48 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1461720358219" ID="ID_1386092182" MODIFIED="1461720378832" TEXT="Universal Distributed System">
+<node CREATED="1461720386770" ID="ID_30330874" MODIFIED="1461720395844" POSITION="right" TEXT="Functional decomposition">
+<node CREATED="1461720400072" ID="ID_509077120" MODIFIED="1461720405309" TEXT="Server">
+<node CREATED="1461720407490" ID="ID_1956925297" MODIFIED="1461720412967" TEXT="Responisbilities">
+<node CREATED="1461720752874" ID="ID_1652375291" MODIFIED="1461720762707" TEXT="Connect to other servers">
+<node CREATED="1461720422410" ID="ID_116905688" MODIFIED="1461720545489" TEXT="Retrieve server lists from other servers"/>
+</node>
+<node CREATED="1461720546917" ID="ID_1632218646" MODIFIED="1461720562240" TEXT="Accept connections from other Servers"/>
+<node CREATED="1461720562875" ID="ID_936663654" MODIFIED="1461720586020" TEXT="Accept connections from Workstations"/>
+<node CREATED="1461720586607" ID="ID_105888399" MODIFIED="1461720597873" TEXT="Accept connections from Clients"/>
+</node>
+</node>
+<node CREATED="1461720601300" ID="ID_417672851" MODIFIED="1461720606138" TEXT="Workstations">
+<node CREATED="1461720608927" ID="ID_1333743887" MODIFIED="1461720615792" TEXT="Responsibilities">
+<node CREATED="1461720639503" ID="ID_180775662" MODIFIED="1461720906956" TEXT="Connect to Server">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1461720787624" ID="ID_1917055765" MODIFIED="1461721056359" TEXT="Receive messages">
+<icon BUILTIN="Mail"/>
+</node>
+<node CREATED="1461720797103" ID="ID_917176435" MODIFIED="1461721064030" TEXT="Send messages">
+<icon BUILTIN="Mail"/>
+</node>
+<node CREATED="1461720886785" ID="ID_162765598" MODIFIED="1461720909420" TEXT="Disconnect from Server">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+<node CREATED="1461720618172" ID="ID_1513475838" MODIFIED="1461720626810" TEXT="Client">
+<node CREATED="1461720628214" ID="ID_755854863" MODIFIED="1461720635313" TEXT="Responsibilities">
+<node CREATED="1461720648033" ID="ID_923131290" MODIFIED="1461720912337" TEXT="Connect to server">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1461720808007" ID="ID_1865532089" MODIFIED="1461721071538" TEXT="Send messages">
+<icon BUILTIN="Mail"/>
+<node CREATED="1461720977601" ID="ID_94214261" MODIFIED="1461720983573" TEXT="Send requests"/>
+<node CREATED="1461720984530" ID="ID_1696809270" MODIFIED="1461720991863" TEXT="Pull responses"/>
+</node>
+<node CREATED="1461720931619" ID="ID_421020040" MODIFIED="1461720948602" TEXT="Disconnect from Server">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
