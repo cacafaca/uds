e99553ca4c9de4a6b248c1d88645461703ce408a
cacafaca <nemanja.simovic@gmail.com>
2016-04-28 03:15:00 +0200

    Initial commit.

---
+++ udscs/FrameworkInterfaces/IConnectable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkInterfaces
+{
+    public interface IConnectable
+    {
+        void Connect(IPEndPoint ipAddress);
+        void Disconnect(IPEndPoint ipAddress);
+    }
+}
---
+++ udscs/Model/BaseInstanceModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDS.Framework.Model
+{
+    public abstract class BaseInstanceModel
+    {
+        private Guid LocalId;
+
+        public Guid Id
+        {
+            get { return LocalId; }
+            set { LocalId = value; }
+        }
+
+        private string LocalName;
+
+        public string Name
+        {
+            get { return LocalName; }
+            set { LocalName = value; }
+        }
+
+        private IPEndPoint LocalAddress;
+
+        public IPEndPoint Address
+        {
+            get { return LocalAddress; }
+            set { LocalAddress = value; }
+        }
+
+    }
+}
