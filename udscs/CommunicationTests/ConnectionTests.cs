f53de8c532e369f2a0d5c1c175bfdbb10076f405
cacafaca <nemanja.simovic@gmail.com>
2016-04-28 16:35:27 +0200

    New test.

--- udscs/CommunicationTests/ConnectionTests.cs
+++ udscs/CommunicationTests/ConnectionTests.cs
@@ -14,7 +14,7 @@ namespace Communication.Tests
     public class ConnectionTests
     {
         [TestMethod()]
-        public void ListenTest()
+        public void ListenAndAcceptTest()
         {
             IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = ipHostInfo.AddressList.Where(adr => adr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).First();
@@ -32,10 +32,43 @@ namespace Communication.Tests
 
                 Socket receiver = connection.Accept();
                 byte[] receivedData = new byte[256];
-                int x = receiver.Receive(receivedData);
+                int receivedCount = receiver.Receive(receivedData);
 
-                Assert.AreEqual(expectedData.Length, x, "Size is different.");
-                for (int i = 0; i < x; i++)
+                Assert.AreEqual(expectedData.Length, receivedCount, "Size is different.");
+                for (int i = 0; i < receivedCount; i++)
+                    Assert.AreEqual(expectedData[i], receivedData[i], "Data are not the same.");
+            }
+            else
+                Assert.Fail("Can't obtain IP address.");
+        }
+
+        [TestMethod()]
+        public void ListenTest()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList.Where(adr => adr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).First();
+            if (ipAddress != null)
+            {
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+                Connection connection = new Connection();
+                connection.Listen(localEndPoint);
+
+                byte[] expectedData = new byte[5] { 1, 2, 3, 4, 5 };
+                new Task(() =>
+                   {
+                       Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                       System.Threading.Thread.Sleep(1000);
+                       sender.Connect(localEndPoint);
+                       sender.Send(expectedData);
+                   }).Start();
+
+                Socket receiver = connection.Accept();
+                byte[] receivedData = new byte[256];
+                int receivedCount = receiver.Receive(receivedData);
+
+                Assert.AreEqual(expectedData.Length, receivedCount, "Size is different.");
+                for (int i = 0; i < receivedCount; i++)
                     Assert.AreEqual(expectedData[i], receivedData[i], "Data are not the same.");
             }
             else
